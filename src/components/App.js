acf0aabedfd684ee14af0585faceeafb0d0ba9ea
dmorales <darwin.morales.tech@gmail.com>
2017-06-29 12:56:03 +1000

    added react-select

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import Footer from './Footer';
 import AddTodo from './AddTodo';
 import VisibleTodoList from './VisibleTodoList';
+import Select from 'react-select';
+
+const options = [
+    {value: 'one', label: 'One'},
+    {value: 'two', label: 'Two'}
+];
+
+function logChange(val) {
+    console.log("Selected: " + JSON.stringify(val));
+}
 
 const App = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
+    <div>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+        <Select
+            name="form-field-name"
+            value="one"
+            options={options}
+            onChange={logChange}
+        />
+    </div>
 );
 
 export default App;
